46e32b7858ed0ed06c1b930c78759de9cd1abed0
MahmoudRamadanDev <mahmoudrmadan565@gmail.com>
2025-03-21 10:01:05 +0200

    add add_note_cubit_states

--- lib/widgets/add_note_modal_sheet.dart
+++ lib/widgets/add_note_modal_sheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubit/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubit/add_note_cubit/add_note_cubit_states.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_faild.dart';
 import 'package:notes_app/widgets/form_add_buttom_sheet.dart';
@@ -8,13 +12,23 @@ class AddNoteModalBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: FormAddButtomSheet(),
+        child: BlocConsumer<AddNoteCubit, AddNoteCubitStates>(
+          listener: (context, state) {
+            if (state is AddNotesFailer) {
+              print("Fialer note");
+            }
+            if (state is AddNotesSuccesfull) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(inAsyncCall: state is AddNotesLoading ? true : false,child: FormAddButtomSheet());
+          },
+        ),
       ),
     );
   }
 }
-
-
